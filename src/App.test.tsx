66d60a7251eab795f0a0f7a9ed64e1bde69c247e
Diana Rodrigues <diana.rodrigues@newhotel.com>
2022-04-06 11:20:13 +0100

    unit tests added;

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// jest.mock("axios");
+
+describe("App", () => {
+  // Unit tests
+  test("renders The Movie DB title", () => {
+    render(<App />);
+    // screen.debug();
+
+    expect(screen.getByText(/The Movie DB/));
+  });
+
+  // Integrations tests
+  test("search for Bohemian movie", async () => {
+    // const movies = [
+    //   { id: "1", title: "Bohemian Rhapsody", year: "2018" },
+    //   { id: "2", title: "The Story of Bohemian Rhapsody", year: "2004" }
+    // ];
+    // const handleSearchChange = jest.fn();
+
+    // render(<App />);
+
+    // await userEvent.type(screen.getByRole("textbox"), "Bohemian");
+
+    // expect(handleSearchChange).toHaveBeenCalledTimes(8);
+
+    // (axios.get as jest.Mock).mockImplementationOnce(() =>
+    //   Promise.resolve({ data: { movies } })
+    // );
+
+    // screen.debug();
+  });
 });
